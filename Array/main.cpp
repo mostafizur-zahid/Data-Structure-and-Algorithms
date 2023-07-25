2a71180f16ae5dbe5208bb70f01059fe8f4fbde0
MD MOSTAFIZUR RAHMAN ZAHID <88180630+mostafizur-zahid@users.noreply.github.com>
2023-07-25 23:53:07 +0600

    update

---
+++ Array/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int A[5] = {1,2,3,4,5};
+
+    cout<<"Size of this array is:"<<sizeof(A)<<endl;
+    for(int x:A){
+        cout<<x<<endl;
+    }
+    
+
+}
